c96190773fc954f81796e3bba458479e1a0e86c7
aW3ikun <socsgogo1@gmail.com>
2021-08-20 13:06:53 +0800

    第二章 通过驱动对象和符号链接 让r3程序访问符号链接对象，使用IRP注册的函数进行交互。

--- NewDriver/main.c
+++ NewDriver/main.c
@@ -1,23 +1,117 @@
 #include<ntddk.h>
 
+#define DEVICE_NAME L"\\Device\\MyFirstDevice"
+//符号链接命名规则
+#define SYM_NAME L"\\??\\MyFirstDevice"
 void nothing(HANDLE hPpid, HANDLE hMypid, BOOLEAN bCreate) {
 	DbgPrint("ProcessNotify\n");
 }
 
+//安全的卸载
 void DrvierUnload(PDRIVER_OBJECT pDriverObject) {
 	DbgPrint("Unload\n");
-	PsSetCreateProcessNotifyRoutine(nothing, TRUE);
+	
+	if (pDriverObject->DeviceObject) {
+		IoDeleteDevice(pDriverObject->DeviceObject);
+
+		UNICODE_STRING sysname = { 0 };
+
+		RtlInitUnicodeString(&sysname, SYM_NAME);
+		IoDeleteSymbolicLink(&sysname);
+	}
 }
 
+// 
+//Dispatch
+//
 
+//使用设备对象，IRP函数
+NTSTATUS MyCreate(PDEVICE_OBJECT pDevice, PIRP pIRP) {
+	NTSTATUS status = STATUS_SUCCESS;
+
+	DbgPrint("My Device has be opened\n");
+
+	pIRP->IoStatus.Status = status;
+
+	pIRP->IoStatus.Information = 0;
+
+	IoCompleteRequest(pIRP, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS MyClose(PDEVICE_OBJECT pDevice, PIRP pIRP) {
+	NTSTATUS status = STATUS_SUCCESS;
+
+	DbgPrint("My Device has be Closed\n");
+
+	pIRP->IoStatus.Status = status;
+
+	pIRP->IoStatus.Information = 0;
+
+	IoCompleteRequest(pIRP, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS MyClean(PDEVICE_OBJECT pDevice, PIRP pIRP) {
+	NTSTATUS status = STATUS_SUCCESS;
+
+	DbgPrint("My Device has be Cleaned\n");
+
+	pIRP->IoStatus.Status = status;
+
+	pIRP->IoStatus.Information = 0;
+
+	IoCompleteRequest(pIRP, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+}
+
+// 使用驱动对象
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath)
 {
 	pDriverObject->DriverUnload = DrvierUnload;
 	NTSTATUS NtStatus = STATUS_SUCCESS;
 
-	DbgPrint("Kernel Load!\n");
-	DbgPrint("---- %wZ -----\n",pRegistryPath);
-	PsSetCreateProcessNotifyRoutine(nothing, FALSE);
+	UNICODE_STRING DeviceName = { 0 };
+
+	PDEVICE_OBJECT pDevice = NULL;
+	//标准流程 初始化
+	RtlInitUnicodeString(&DeviceName, DEVICE_NAME);
+	//能不能用其他驱动对象创建device
+	NtStatus = IoCreateDevice(pDriverObject, 0, &DeviceName, FILE_DEVICE_UNKNOWN, 0, TRUE, &pDevice);
+
+	//打开其他驱动对象
+	//NtStatus = IoCreateDevice((PDRIVER_OBJECT)0xFFFFF80643A50000, 0, &DeviceName, FILE_DEVICE_UNKNOWN, 0, TRUE, &pDevice);
+
+	if (!NT_SUCCESS(NtStatus)) {
+		DbgPrint("Create Device Failed :%x\n", NtStatus);
+
+		return NtStatus;
+	}
+
+	//
+	// 创建设备成功 创建符号链接
+	//
+	UNICODE_STRING symname = { 0 };
+
+	RtlInitUnicodeString(&symname, SYM_NAME);
+
+	NtStatus = IoCreateSymbolicLink(&symname, &DeviceName);
+
+	if (!NT_SUCCESS(NtStatus)) {
+
+		DbgPrint("Create SymbolicLink Failed:%x\n", NtStatus);
+		IoDeleteDevice(pDevice);
+		return NtStatus;
+	}
+
+	pDriverObject->MajorFunction[IRP_MJ_CREATE] = MyCreate;
+
+	pDriverObject->MajorFunction[IRP_MJ_CLOSE] = MyClose;
+
+	pDriverObject->MajorFunction[IRP_MJ_CLEANUP] = MyClean;
 
 	return NtStatus;
 }
---
+++ r3Consle/r3Consle.cpp
@@ -0,0 +1,38 @@
+﻿// r3Consle.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<Windows.h>
+
+
+int main()
+{
+    HANDLE hDevice = NULL;
+    CHAR readbuffer[50] = { 0 };
+    DWORD dwRead = 0;
+    DWORD dwWrite = 0;
+    hDevice = CreateFile(L"\\\\\.\\MyFirstDevice", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (hDevice==INVALID_HANDLE_VALUE) {
+        std::cout << "Open Device Failed\n";
+        system("pause");
+        return 0;
+    }
+
+    std::cout << "Open Success!\n";
+    system("pause");
+    
+    // ReadFile(hDevice, readbuffer, 50, &dwRead, NULL);
+
+    // printf("--%p--%s--%d--\n",readbuffer, readbuffer, dwRead);
+    // system("pause");
+
+    // WriteFile(hDevice, "This Message Come From R3", strlen("This Message Come From R3"), &dwWrite,NULL);
+
+    // printf("--%d--\n", dwWrite);
+    CloseHandle(hDevice);
+    system("pause");
+       
+    return 0;
+}
+
