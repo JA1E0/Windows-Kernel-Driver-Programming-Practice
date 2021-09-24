cfc32988fa9aec989f77c61045be4287a065b94c
aW3ikun <socsgogo1@gmail.com>
2021-09-24 10:49:00 +0800

    课16 根据序号获取SSDT函数地址

--- SSDT_HOOK/main.c
+++ SSDT_HOOK/main.c
@@ -1,4 +1,5 @@
 #include<ntifs.h>
+#include<windef.h>
 
 //Windows7 sp1
 
@@ -27,12 +28,26 @@
 //00007FF95BB1093F | 1111 | adc dword ptr ds : [rcx] , edx |
 //00007FF95BB10941 | 1111 | adc dword ptr ds : [rcx] , edx |
 
-typedef NTSTATUS (*pNtOpenProcess)(
+
+//nt!KiSystemServiceRepeat:
+//fffff800`03e98772 4c8d15c7202300  lea     r10, [nt!KeServiceDescriptorTable(fffff800`040ca840)]
+//fffff800`03e98779 4c8d1d00212300  lea     r11, [nt!KeServiceDescriptorTableShadow(fffff800`040ca880)]
+//fffff800`03e98780 f7830001000080000000 test dword ptr[rbx + 100h], 80h
+//fffff800`03e9878a 4d0f45d3        cmovne  r10, r11
+
+typedef NTSTATUS(*pNtOpenProcess)(
 	PHANDLE            ProcessHandle,
 	ACCESS_MASK        DesiredAccess,
 	POBJECT_ATTRIBUTES ObjectAttributes,
 	PCLIENT_ID         ClientId
-);
+	);
+
+typedef struct _KSERVICE_TABLE_DESCRIPTOR {
+	PULONG_PTR ServiceTableBase;	//SSDT基址
+	PVOID ServiceCounterTableBase;	//SSDT中服务被调用次数计数器
+	ULONG NumberOfService;	//SSDT服务个数
+	PUCHAR ParamTableBase;
+} KSERVICE_TABLE_DESCRIPTOR, * PKSERVICE_TABLE_DESCRIPTOR;
 
 PVOID pOldCode = NULL;
 
@@ -40,37 +55,61 @@ PULONG64 ReturnCode = NULL;
 
 PVOID pApiAddr = NULL;
 
+BOOLEAN g_HookFlag = FALSE;
+
 NTSTATUS MyNtOpenProcess(PHANDLE ProcessHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, PCLIENT_ID ClientId);
 
 KIRQL	UpIRQL();
 
 VOID	DownIRQL(KIRQL OldIrql);
 
+NTSTATUS	HookNtOpen();
+
 VOID	RestoreCode();
 
-VOID	DriverUnload(PDRIVER_OBJECT pDriverObject)
-{
+NTSTATUS GetSSDTFunc();
+
+VOID	DriverUnload(PDRIVER_OBJECT pDriverObject) {
 	DbgPrint("UnLoad\n");
-	//恢复钩子
-	RestoreCode();
-	//释放创建的内存
-	ExFreePool(pOldCode, 0x100);
+
+	if (g_HookFlag) {
+		//恢复钩子
+		RestoreCode();
+		//释放创建的内存
+		ExFreePool(pOldCode, 0x100);
+	}
 
 	return;
 }
 
-NTSTATUS	DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
-{
+NTSTATUS	DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath) {
 	pDriverObject->DriverUnload = DriverUnload;
 
 	NTSTATUS status = STATUS_SUCCESS;
 
+	//status = HookNtOpen();
+
+	////Hook NtOpenProcess
+	//if (NT_SUCCESS(status)) {
+	//	g_HookFlag = TRUE;
+	//}
+
+	//获取SSDT
+	status = GetSSDTFunc();
+
+
+	return status;
+}
+
+NTSTATUS HookNtOpen() {
+	NTSTATUS status = STATUS_SUCCESS;
+
 	CHAR JmpCode[12] = { 0x48,0xB8,0x22,0x22,0x22,0x22,0x11,0x11,0x11,0x11,0xFF,0xE0 };
 
-	CHAR SpringBoard[14] = { 0xFF,0x25,0x00,0x00,0x00,0x00,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11};
+	CHAR SpringBoard[14] = { 0xFF,0x25,0x00,0x00,0x00,0x00,0x11,0x11,0x11,0x11,0x11,0x11,0x11,0x11 };
 
 	UNICODE_STRING puApiName = { 0 };
-	
+
 	KIRQL OldIrql = 0;
 
 	RtlInitUnicodeString(&puApiName, L"NtOpenProcess");
@@ -82,8 +121,8 @@ NTSTATUS	DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 	}
 
 	//构建shellcode
-	*((PULONG64)(JmpCode+2)) = (PULONG64)MyNtOpenProcess;
-	
+	*((PULONG64)(JmpCode + 2)) = (PULONG64)MyNtOpenProcess;
+
 	*((PULONG64)(SpringBoard + 6)) = (ULONG64)pApiAddr + 13;
 
 	pOldCode = ExAllocatePool(NonPagedPool, 0x100);
@@ -98,7 +137,7 @@ NTSTATUS	DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 
 	//辅助跳板构建
 	RtlCopyMemory((PVOID)((ULONG64)pOldCode + 13), SpringBoard, sizeof(SpringBoard));
-	
+
 	//提升中断权限
 	OldIrql = UpIRQL();
 	//Hook
@@ -110,11 +149,11 @@ NTSTATUS	DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 	return status;
 }
 
-NTSTATUS MyNtOpenProcess(PHANDLE ProcessHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, PCLIENT_ID ClientId)
-{
-	if (ClientId->UniqueProcess == 2222) {
+NTSTATUS MyNtOpenProcess(PHANDLE ProcessHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, PCLIENT_ID ClientId) {
+	if (ClientId->UniqueProcess == 3624) {
 		return STATUS_ACCESS_DENIED;
 	}
+
 	//DbgPrint("OpenProcess Pid:%d\n", ClientId->UniqueProcess);
 
 	pNtOpenProcess pFunc = pOldCode;
@@ -125,8 +164,7 @@ NTSTATUS MyNtOpenProcess(PHANDLE ProcessHandle, ACCESS_MASK DesiredAccess, POBJE
 //提升中断权限，防止被打断
 //关闭写保护 操作Cr0标志位
 //关中断 当前执行不被中断
-KIRQL UpIRQL()
-{
+KIRQL UpIRQL() {
 	KIRQL OldIrql = KeRaiseIrqlToDpcLevel();
 
 	UINT64 cr0 = __readcr0();
@@ -139,11 +177,11 @@ KIRQL UpIRQL()
 
 	return OldIrql;
 }
+
 //恢复中断短线
 //打开写保护
 //开中断
-VOID DownIRQL(KIRQL OldIrql)
-{
+VOID DownIRQL(KIRQL OldIrql) {
 	KeLowerIrql(OldIrql);
 
 	UINT64 cr0 = __readcr0();
@@ -156,6 +194,7 @@ VOID DownIRQL(KIRQL OldIrql)
 
 	return;
 }
+
 VOID	RestoreCode() {
 
 	KIRQL OldIrql = UpIRQL();
@@ -164,4 +203,41 @@ VOID	RestoreCode() {
 
 	DownIRQL(OldIrql);
 	return;
+}
+
+
+NTSTATUS GetSSDTFunc() {
+	NTSTATUS	status = STATUS_SUCCESS;
+	PUCHAR	pSystemCall = 0;
+	ULONG	uCodeOffset = 0;
+	ULONG	uFuncOffset = 0;
+	PULONG	uFuncNum = 0;
+	DWORD dwNum = 2;
+	PKSERVICE_TABLE_DESCRIPTOR KeServiceDescriptorTable = NULL;
+	PULONG_PTR pBaseAddr = NULL;
+	ULONG64 pFuncAddr = NULL;
+
+
+	pSystemCall = (PUCHAR)__readmsr(0xC0000082);
+
+	//.text:FFFFF80003E98772                                         KiSystemServiceRepeat:                  ; CODE XREF: KiSystemCall64+47B↓j
+	//.text:FFFFF80003E98772 000 4C 8D 15 C7 20 23 00                lea     r10, KeServiceDescriptorTable
+	for (int i = 0; i < 1000; i++) {
+		if (*(pSystemCall + i) == 0x4c && *(pSystemCall + i + 1) == 0x8D && *(pSystemCall + i + 2) == 0x15) {
+			pSystemCall += i;
+			uCodeOffset = *((PULONG32)(pSystemCall + 3));
+			KeServiceDescriptorTable = (PKSERVICE_TABLE_DESCRIPTOR)((ULONG64)pSystemCall + 7 + uCodeOffset);
+
+			break;
+		}
+	}
+
+	pBaseAddr = KeServiceDescriptorTable->ServiceTableBase;
+	uFuncNum = (PULONG)pBaseAddr;
+	uFuncOffset = uFuncNum[dwNum] >> 4;
+	pFuncAddr = (ULONG64)pBaseAddr + uFuncOffset;
+
+	DbgPrint("Function Address: %p\n", pFuncAddr);
+
+	return status;
 }
